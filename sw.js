a17812858e823affdc3d24720471ffb3d35ec907
cbdowney09-alt <cbdowney09@gmail.com>
2025-11-13 21:52:24 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('SW registered', reg))
+    .catch(err => console.log('SW registration failed', err));
+}
+
+const CACHE_NAME = 'dailybread-v1';
+const urlsToCache = ['/', '/index.html', '/styles.css', '/script.js', '/icons/icon-192x192.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
